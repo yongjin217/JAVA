82f871554be5a126f0ef9a3a146407934ef3faa7
yongjin217 <abcd950217@gmail.com>
2023-06-08 23:38:52 +0900

    study : 출력

---
+++ java/src/ch02/Ex2_1.java
@@ -0,0 +1,11 @@
+package ch02;
+
+public class Ex2_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world"); // 출력하고 줄바꿈 o
+        System.out.print("Hello");         // 출력하고 줄바꿈 x
+        System.out.println("world");       // 출력하고 줄바꿈 o
+    }
+
+
+}
