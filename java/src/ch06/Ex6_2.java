a10fce94f0ed368e3aba60882caf91ca1ecaa8df
yongjin217 <abcd950217@gmail.com>
2023-06-12 20:12:18 +0900

    study : 클래스의 또 다른 정의

---
+++ java/src/ch06/Ex6_2.java
@@ -0,0 +1,20 @@
+package ch06;
+
+class Time {
+    int hour;
+    int minute;
+    int second;
+}
+
+public class Ex6_2 {
+    public static void main(String[] args) {
+        Tv[] tvArr = new Tv[3];
+
+        tvArr[0] = new Tv();
+        tvArr[1] = new Tv();
+        tvArr[2] = new Tv();
+
+        //Tv[] tvArr = { new Tv(), new Tv(), new Tv() };
+
+    }
+}
